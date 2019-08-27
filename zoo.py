07421b76f1c72f96156923a528db37ada97da046
Misty DeRamus <mistyderamus@gmail.com>
2019-08-27 08:56:09 -0500

    convert tuple to list

--- zoo.py
+++ zoo.py
@@ -6,6 +6,7 @@ if animal_to_find in zoo:
     print(animal_to_find.capitalize(), " is in the list.")
 else:
         print(animal_to_find.capitalize(), "is not in the list.")
+
 (first_animal,second_animal,third_animal,fourth_animal,fifth_animal,sixth_animal,seventh_animal,eighth_animal,nineth_animal,tenth_animal) = zoo
 print (first_animal)
 print (second_animal)
@@ -18,6 +19,9 @@ print (eighth_animal)
 print (nineth_animal)
 print (tenth_animal)
 # convert tuple to a list
-print(list(zoo))
-new_animal = ("iguana","racoon")
-new_animal.extend(fred)
+zoo = list(zoo)
+print(f'Zoo is now a list: ', list(zoo))
+new_animal = ["iguana","racoon","kangaroo"]
+# Use extend() to add three more animals to your zoo
+zoo.extend(new_animal)
+print(f'Zoo has been extended with 3 new animals!', zoo)
